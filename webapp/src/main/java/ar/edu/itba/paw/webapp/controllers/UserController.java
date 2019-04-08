c31643e215eafe19d55ce6d5ad749ca6b627543d
Maite <mherran@itba.edu.ar>
2019-04-08 01:56:57 -0300

    Mapeo botón unirse a la changa con método joinChanga

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -79,6 +80,11 @@ public class UserController {
                 .withEmail(form.getUsername())
                 .withPasswd(form.getPassword())
                 .build()).getValue();
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "/joinChanga", method = RequestMethod.POST)
+    public ModelAndView showChanga() {
         return new ModelAndView("redirect:/");
     }
 
