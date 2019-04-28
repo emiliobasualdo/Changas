77dce1173ccd26245a67f6e1614b4b149cfb68fc
Maite <mherran@itba.edu.ar>
2019-04-28 03:50:25 -0300

    send email

--- service/src/main/java/ar/edu/itba/paw/services/EmailServiceImplementation.java
+++ service/src/main/java/ar/edu/itba/paw/services/EmailServiceImplementation.java
@@ -5,6 +5,8 @@ import ar.edu.itba.paw.models.Changa;
 import ar.edu.itba.paw.models.User;
 import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,9 @@ public class EmailServiceImplementation implements EmailService{
 
     @Autowired
     public JavaMailSender emailSender;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     public void sendEmail(String to, String subject, String body) {
@@ -26,11 +31,12 @@ public class EmailServiceImplementation implements EmailService{
 
     @Override
     public void sendJoinRequestEmail(Changa changa, User changaOwner, User requestingUser) {
-        sendEmail(changaOwner.getEmail(), "Solicitud", joinRequestEmailBody(changa, changaOwner, requestingUser));
+        String subject = messageSource.getMessage("sendJoinRequestSubject", null, LocaleContextHolder.getLocale());
+        sendEmail(changaOwner.getEmail(), subject, joinRequestEmailBody(changa, changaOwner, requestingUser));
     }
 
 
-    //TODO INTERNACIONALIZAR
+    //TODO HACER CON HTML E INTERNACIONALIZADO
     private String joinRequestEmailBody(Changa changa, User changaOwner, User currentUser){
         StringBuilder sb = new StringBuilder();
         sb.append(changaOwner.getName());
